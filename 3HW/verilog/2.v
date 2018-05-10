77d3660c8b05e18fd748edc28f66ab0b398b274c
Andy19961017 <b04901067@ntu.edu.tw>
2018-05-10 19:31:09 +0800

    a

--- 3HW/verilog/2.v
+++ 3HW/verilog/2.v
@@ -155,7 +155,7 @@ module alu(
     output reg  zero;
     output reg [31:0] out;
     wire [31:0] add;
-module carry_lookahead(x, y, 0, add, CarryOut);
+carry_lookahead adder(x, y, 0, add, CarryOut);
 
     always@(*) begin
         case(ctrl) //synopsys full_case
@@ -327,44 +327,11 @@ module register_file(
     end
 endmodule
 
-`timescale 1ns / 1ps
-module carry_look_ahead_16bit(a,b, cin, sum,cout);
-input [15:0] a,b;
-input cin;
-output [15:0] sum;
-output cout;
-wire c1,c2,c3;
- 
-carry_look_ahead_4bit cla1 (.a(a[3:0]), .b(b[3:0]), .cin(cin), .sum(sum[3:0]), .cout(c1));
-carry_look_ahead_4bit cla2 (.a(a[7:4]), .b(b[7:4]), .cin(c1), .sum(sum[7:4]), .cout(c2));
-carry_look_ahead_4bit cla3(.a(a[11:8]), .b(b[11:8]), .cin(c2), .sum(sum[11:8]), .cout(c3));
-carry_look_ahead_4bit cla4(.a(a[15:12]), .b(b[15:12]), .cin(c3), .sum(sum[15:12]), .cout(cout));
- 
-endmodule
  
 ////////////////////////////////////////////////////////
 //4-bit Carry Look Ahead Adder 
 ////////////////////////////////////////////////////////
  
-module carry_look_ahead_4bit(a,b, cin, sum,cout);
-input [:0] a,b;
-input cin;
-output [3:0] sum;
-output cout;
- 
-wire [3:0] p,g,c;
- 
-assign p=a^b;//propagate
-assign g=a&b; //generate
- 
-assign c[0]=cin;
-assign c[1]= g[0]|(p[0]&c[0]);
-assign c[2]= g[1] | (p[1]&g[0]) | p[1]&p[0]&c[0];
-assign c[3]= g[2] | (p[2]&g[1]) | p[2]&p[1]&g[0] | p[2]&p[1]&p[0]&c[0];
-assign cout= g[3] | (p[3]&g[2]) | p[3]&p[2]&g[1] | p[3]&p[2]&p[1]&p[0]&c[0];
-assign sum=p^c;
- 
-endmodule
 
 module carry_lookahead(p, g, CarryIn, c, CarryOut);
     input [31:0] p, g;
@@ -374,7 +341,7 @@ module carry_lookahead(p, g, CarryIn, c, CarryOut);
     
     genvar i;
     generate
-    for(i=0; i<31; i++)
+    for(i=0; i<31; i=i+1)
         begin: CarryLookAhead
             cal_carry myCarry(p[i], g[i], c[i], c[i+1]);
         end
--- 3HW/verilog/INCA_libs/irun.lnx86.15.20.nc/.timestamp.ts
+++ 3HW/verilog/INCA_libs/irun.lnx86.15.20.nc/.timestamp.ts
@@ -1,6 +1,7 @@
 1525857614 /home/raid7_2/userb04/b04067/DSD/3HW/verilog/DSDHW3.v
-1525881077 /home/raid7_2/userb04/b04067/DSD/3HW/verilog/DSDHW3.vg
+1525951753 /home/raid7_2/userb04/b04067/DSD/3HW/verilog/DSDHW3.vg
 1525762873 /home/raid7_2/userb04/b04067/DSD/3HW/verilog/DSDHW3_tb.v
 1525850180 /home/raid7_2/userb04/b04067/DSD/3HW/verilog/tsmc13.v
-1525881247 /home/raid7_2/userb04/b04067/DSD/3HW/verilog/DSDHW3_tb_gate.v
+1525951826 /home/raid7_2/userb04/b04067/DSD/3HW/verilog/DSDHW3_tb_gate.v
+1525885397 /home/raid7_2/userb04/b04067/DSD/3HW/verilog/2.v
 1525710554 /home/raid7_2/userb04/b04067/DSD/3HW/verilog/HSs18n_128x32.v
